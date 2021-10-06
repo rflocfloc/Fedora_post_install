a9648f6d41d21a3595e8fa9fb2b39b94cb707455
Riccardo Scandino <43863486+rfloc95@users.noreply.github.com>
2021-10-06 17:08:24 +0200

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,3 +1,15 @@
 #!bin/bash
 
-flatpak install -y Apostrophe Firmware FontDownloader Fragments GIMP Inkscape PDFArranger Telegram Spotify
+flatpak install -y \
+                  Firmware \              # lets you check and update firmware of your hardware
+                  FontDownloader \        
+                  Fragments \             # Torrents download client 
+                  GIMP \ 
+                  GIMP.Plugins.Resynthetizer \    # Plugin for GIMP heal correction 
+                  Inkscape \ 
+                  PDFArranger \ 
+                  Apostrophe \            # Markdown editor
+                  Telegram \ 
+                  Spotify \ 
+                  Giara \                 # Reddit client app
+                  Cawbird                 # Twitter client app
